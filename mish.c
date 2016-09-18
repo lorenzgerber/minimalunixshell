4edfcaa7b0336354c006092f508743734cd62aeb
Lorenz Gerber <lorenz.gerber@lablicate.com>
2016-09-18 09:23:20 +0200

    added pseudo code from lecture

--- mish.c
+++ mish.c
@@ -33,3 +33,31 @@ int main() {
     printf("hello world\n");
     return 0;
 }
+
+
+
+/*
+ *  below a draft of the main processing section according to the seminar
+ *  
+ *  for command in commandline
+ *      if there is a next command
+ *          pipe()
+ *      fork()
+ *      if child:
+ *          if previous command exists:
+ *              duplicate old read end to current read end
+ *              close unused one
+ *          if next command exists:
+ *              duplicate new write end to current write end
+ *              close unused one
+ *          #redirects#
+ *          execute command
+ *      else:
+ *          if previous command exists:
+ *              clean up
+ *          if next command exists:
+ *              move file descriptor
+ *          if multiple commands exist:
+ *              close all resources
+ *
+ */
