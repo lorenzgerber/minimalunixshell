e58bf3eef372f0b27dd8e845c803e80ce14f1b8d
Lorenz Gerber <lorenz.gerber@lablicate.com>
2016-09-18 09:14:49 +0200

    checkin all needed files

---
+++ mish.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+
+    /*
+     * Initialize variables
+     */
+
+    /*
+     * Produce Prompt
+     */
+
+    /*
+     * Parse input
+     */
+
+    /*
+     * Dispatch internal commands
+     */
+
+    /*
+     * Dispatch to external commands
+     */
+
+    /*
+     * Stop or kill current running processes
+     */
+
+
+
+
+
+    printf("hello world\n");
+    return 0;
+}
